432fc7a90be8d507f3f236bab44ca44ec3d0db4c
Andrew Njoo <adnjoo@gmail.com>
2021-07-26 13:35:06 -0700

    add controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+expense = Expense.create([
+  {
+    date: '07-25-2021',
+    category: 'boba',
+    detail: "pop tea bar",
+    expense: 6.00
+  },
+  {
+    date: '07-26-2021',
+    category: 'eating out',
+    detail: "Alberto's gyro",
+    expense: 28.00
+  }
+])
